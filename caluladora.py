040468819c7436be8234361b58c560679d84b272
EdsonLopes95 <116630766+EdsonLopes95@users.noreply.github.com>
2022-10-25 05:20:29 -0300

    Add files via upload

---
+++ caluladora.py
@@ -0,0 +1,23 @@
+#importando tkinter
+
+from tkinter import *
+from tkinter import ttk
+
+#cores para calculadora
+
+cor1 = '#f9faeb' #branco
+#cor2 =
+#cor3 =
+#cor4 =
+#cor5 =
+#cor6 =
+
+#janela em desenvolvimento
+
+janela = Tk()
+janela.title('Calculadora')
+janela.geometry('300x400')
+
+
+
+janela.mainloop()
